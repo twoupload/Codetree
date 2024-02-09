b2e298b55f96b18365ffd37fd6a5f36d197dc1e6
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-09 10:31:25 +0900

    Title: 사각형의 총 넓이 2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240209/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#define OFFSET 100
+
+using namespace std;
+
+int a[201][201];
+int c;
+
+int main()
+{
+    //freopen("input.txt" ,"r", stdin);
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        int x, xx, y, yy,lx,ly;
+        cin >> x >> y >> xx >> yy;
+
+        lx = (xx + OFFSET) - (x + OFFSET);
+        ly = (yy + OFFSET) - (y + OFFSET);
+
+        for(int j = y; j < y + ly; j++) {
+            for(int t = x; t < x + lx; t++) {
+                a[j][t] = 1;
+            }
+        }
+    }
+
+    for(int i = 0; i <= 100; i++) {
+        for(int j = 0; j <= 100; j++) {
+            if(a[i][j] != 0) {
+                c++;
+            }
+        }
+    }
+
+    cout << c;
+
+    return 0;
+}
