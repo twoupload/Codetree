d049395980781865cbcce985c8f11a17c74dcb34
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-10 15:56:15 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 14ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240210/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#define OFFSET 1000
+
+using namespace std;
+
+int i,j,t;
+int a[3001][3001];
+int x,x2,y,y2,lx,ly;
+int lenX = -9999, temp, lenY;
+
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+
+    for(i = 1; i <= 2; i++) {
+        cin >> x >> y >> x2 >> y2;
+
+        lx = (x2 + OFFSET) - (x + OFFSET);
+        ly = (y2 + OFFSET) - (y + OFFSET);
+
+        if(i == 1) {
+            for(j = y + OFFSET; j < ly + OFFSET + y; j++) {
+                for(t = x + OFFSET; t < x + OFFSET + lx; t++) {
+                    a[j][t] = 1;
+                }
+            }
+        }
+        else {
+            for(j = y + OFFSET; j < y + OFFSET + ly; j++) {
+                for(t = x + OFFSET; t < x + OFFSET + lx; t++) {
+                    a[j][t] = 0;
+                }
+            }
+        }
+
+    }
+
+    for(i = 0; i <= 2001; i++) {
+        for(j = 0; j <= 2001; j++) {
+            if(a[i][j] == 1) {
+                temp++;
+                lenY = i - OFFSET;
+            }
+        }
+        if(temp > lenX) {
+            lenX = temp;
+        }
+        temp = 0;
+    }
+
+    cout << lenX * lenY;
+    return 0;
+}
