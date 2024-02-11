5e0bbe85bd28b09b32d745b00f5798ad2e9fea0b
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-12 08:20:13 +0900

    Title: 연속되는 수 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int a[1001];
+int dp[1001];
+int n,i,cnt = -9999;
+
+int main()
+{
+    //freopen("input.txt", "r" ,stdin);
+    cin >> n;
+
+    for(i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    dp[1] = 1;
+
+    for(i = 2; i <= n; i++) {
+        if(a[i-1] * a[i] > 0) {
+            dp[i] = dp[i - 1] + 1;
+        }
+        else {
+            dp[i] = 1;
+        }
+    }
+
+    for(i = 1; i <= n; i++) {
+        if(cnt < dp[i]) {
+            cnt = dp[i];
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
