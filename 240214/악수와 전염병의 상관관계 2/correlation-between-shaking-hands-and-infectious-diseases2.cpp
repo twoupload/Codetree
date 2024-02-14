a3ade29e089f1db3ce05ac7bc5dcd7b4f7225641
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-14 10:55:32 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240214/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
+++ 240214/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -44,23 +44,26 @@ int main()
 
     for(i = 1; i <= T; i++) {
         if(infp[info[i].px].sw == true && infp[info[i].py].sw == true) {
-            if(infp[info[i].px].inf == true) {
+            if(infp[info[i].px].inf == true && infp[info[i].py].inf == false) {
                 infp[info[i].px].cnt += 1;
                 infp[info[i].py].inf = true;
             }
+            else if(infp[info[i].px].inf == false && infp[info[i].py].inf == true) {
+                infp[info[i].py].cnt += 1;
+                infp[info[i].px].inf = true;
+            }
             else {
-                if(infp[info[i].py].inf == true) {
-                    infp[info[i].py].cnt += 1;
-                    infp[info[i].px].inf = true;
-                }
-                else continue;
+                infp[info[i].px].cnt += 1;
+                infp[info[i].py].cnt += 1;
             }
         }
         else continue;
 
         if(infp[info[i].px].cnt >= K) infp[info[i].px].sw = false;
         if(infp[info[i].py].cnt >= K) infp[info[i].py].sw = false;
+
     }
+
 
     for(i = 1; i <= N; i++) {
         cout << infp[i].inf;
