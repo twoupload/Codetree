87e6e91340f0953874765f6ef5200b4a04745e35
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-16 07:57:16 +0900

    Title: 방향에 맞춰 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240216/방향에 맞춰 이동/move-in-direction.cpp
+++ 240216/방향에 맞춰 이동/move-in-direction.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
-int main() {
-    // 여기에 코드를 작성해주세요.
+using namespace std;
+
+int dirx[1001], diry[1001];
+int n,m,i;
+char d;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    for(i = 1; i <= n; i++)
+    {
+        cin >> d >> m;
+
+        if(d == 'N')
+            dirx[i] = dirx[i], diry[i] = diry[i-1] + m;
+        else if(d == 'S')
+            dirx[i] = dirx[i-1], diry[i] = diry[i-1] - m;
+        else if(d == 'E')
+            dirx[i] = dirx[i-1] + m, diry[i] = diry[i-1];
+        else
+            dirx[i] = dirx[i-1] - m, diry[i] = diry[i-1];
+    }
+
+    cout << dirx[n] << " " << diry[n];
     return 0;
 }
