8ee085087ddc16aceb4ab1d41da3e7686487e486
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-17 15:15:37 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240217/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
+++ 240217/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -1,41 +1,50 @@
 #include <iostream>
-#include <vector>
+
+#define MAX_NUM 100
+#define DIR_NUM 4
 
 using namespace std;
 
+int n, m;
+int arr[MAX_NUM][MAX_NUM];
+
+int dx[DIR_NUM] = {0, 1,  0, -1};
+int dy[DIR_NUM] = {1, 0, -1,  0};
+
+int curr_x, curr_y;  // 시작은 (0, 0) 입니다.
+int dir;             // 0: 오른쪽, 1: 아래쪽, 2: 왼쪽, 3: 위쪽
+
+bool InRange(int x, int y){
+    return 0 <= x && x < n && 0 <= y && y < m;
+}
+
 int main() {
-    int n, m;
+    // 입력:
     cin >> n >> m;
-    
-    vector<vector<int>> arr(n, vector<int>(m, 0));
-    int dx[4] = {0, 1, 0, -1};
-    int dy[4] = {1, 0, -1, 0};
-    
-    auto in_range = [&](int x, int y) {
-        return 0 <= x && x < n && 0 <= y && y < m;
-    };
-    
-    int x = 0, y = 0;
-    int dir_num = 0;
-    arr[x][y] = 1;
-    
-    for(int i = 2; i <= n * m; i++) {
-        int nx = x + dx[dir_num], ny = y + dy[dir_num];
-        
-        if(!in_range(nx, ny) || arr[nx][ny] != 0) {
-            dir_num = (dir_num + 1) % 4;
-        }
-        
-        x += dx[dir_num], y += dy[dir_num];
-        arr[x][y] = i;
+
+    // 처음 시작 위치에 초기값을 적습니다.
+    arr[curr_x][curr_y] = 1;
+
+    // n*m개의 숫자를 적어야 합니다. 
+    for(int i = 2; i <= n * m; i++) { // 숫자 i를 어디에 적을지 결정합니다.
+        // 현재 방향 dir를 기준으로 그 다음 위치 값을 계산합니다.
+        int next_x = curr_x + dx[dir], next_y = curr_y + dy[dir];
+
+        // 더 이상 나아갈 수 없다면
+        // 시계방향으로 90'를 회전합니다.
+        if(!InRange(next_x, next_y) || arr[next_x][next_y] != 0)
+            dir = (dir + 1) % 4;
+
+        // 그 다음 위치로 이동한 다음 배열에 올바른 값을 채워넣습니다.
+        curr_x = curr_x + dx[dir]; curr_y = curr_y + dy[dir];
+        arr[curr_x][curr_y] = i;
     }
     
+    // 출력:
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            cout << arr[i][j] << ' ';
-        }
-        cout << '\n';
+        for(int j = 0; j < m; j++) 
+            cout << arr[i][j] << " ";
+        cout << endl;
     }
-    
     return 0;
 }
