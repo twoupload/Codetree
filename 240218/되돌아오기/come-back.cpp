206fd05aa644594f8bda6598670c103e1479b5b1
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-18 08:43:25 +0900

    Title: 되돌아오기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240218/되돌아오기/come-back.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#define MAX 1234567
+using namespace std;
+
+int i,j;
+int dx[MAX],dy[MAX];
+
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+    int n,cnt = 1;
+    cin >> n;
+
+    for(i = 1; i <= 6; i++) {
+        char d;
+        int m;
+        cin >> d >> m;
+
+        for(j = cnt; j < cnt + m; j++) {
+            if(d == 'N') {
+                dx[j] = dx[j-1];
+                dy[j] = dy[j-1] + 1;
+            }
+            else if(d == 'S') {
+                dx[j] = dx[j-1];
+                dy[j] = dy[j-1] -1;
+            }
+            else if(d == 'W') {
+                dx[j] = dx[j-1] - 1;
+                dy[j] = dy[j-1];
+            }
+            else {
+                dx[j] = dx[j-1] + 1;
+                dy[j] = dy[j-1];
+            }
+        }
+        cnt = cnt + m;
+    }
+
+    for(i = 1; i < cnt; i++) {
+        if(dx[i] == 0 && dy[i] == 0) {
+            cout << i;
+            break;
+        }
+    }
+    return 0;
+}
