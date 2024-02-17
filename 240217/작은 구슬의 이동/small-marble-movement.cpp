6989b5d6cd01781e84a2028de27df0a54693c6d6
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-17 11:54:58 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240217/작은 구슬의 이동/small-marble-movement.cpp
+++ 240217/작은 구슬의 이동/small-marble-movement.cpp
@@ -1,60 +1,68 @@
 /// 작은 구슬의 이동
 #include <iostream>
-
+#define MAX 1234567
 using namespace std;
 
-int dx[101], dy[101];
-int n,t,r,c,dir,cnt;
-int i;
-char d;
+int dx[MAX], dy[MAX];
 
 int main()
 {
     //freopen("input.txt", "r", stdin);
 
+    int n,t;
     cin >> n >> t;
+
+    int r, c;
+    char d;
     cin >> r >> c >> d;
 
     dx[0] = c, dy[0] = r;
 
-    if(d == 'U') dir = 2;
+    int dir;
+
+    if(d == 'R') dir = 0;
+    else if(d == 'U') dir = 2;
     else if(d == 'D') dir = 1;
-    else if(d == 'R') dir = 0;
     else dir = 3;
 
-    for(i = 1; i <= t; i++)
-    {
-        if( (dir == 3 && dx[i-1] == 1) ||
-            (dir == 0 && dx[i-1] == 4) ||
-            (dir == 2 && dy[i-1] == 1) ||
-            (dir == 1 && dy[i-1] == 4) ) {
-                dir = 3 - dir;
-                dx[i] = dx[i-1];
-                dy[i] = dy[i-1];
-            }
+
+    for(int i = 1; i <= t; i++) {
+        if((dx[i-1] == 1 && dir == 3) || (dx[i-1] == n && dir == 0)
+           || (dy[i-1] == 1 && dir == 2) || (dy[i-1] == n && dir == 1))
+        {
+            dir = 3 - dir;
+            dx[i] = dx[i-1];
+            dy[i] = dy[i-1];
+        }
 
         else
         {
-            if(dir == 0) {
+            if(dir == 0)
+            {
                 dx[i] = dx[i-1] + 1;
                 dy[i] = dy[i-1];
             }
-            else if(dir == 1) {
+            else if(dir == 1)
+            {
                 dx[i] = dx[i-1];
                 dy[i] = dy[i-1] - 1;
             }
-            else if(dir == 2) {
+            else if(dir == 2)
+            {
                 dx[i] = dx[i-1];
                 dy[i] = dy[i-1] + 1;
             }
-            else {
+            else
+            {
                 dx[i] = dx[i-1] - 1;
                 dy[i] = dy[i-1];
             }
         }
     }
 
-    cout << dy[t] << " " << dx[t];
 
+    for(int i = 0; i <= t; i++) {
+        cout << dy[i] << " " << dx[i] << endl;
+    }
     return 0;
 }
