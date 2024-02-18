661a249cc36ce567a75e42db5f7d313ccd0d4b99
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-18 09:06:05 +0900

    Title: 되돌아오기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240218/되돌아오기 2/come-back-2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+int i;
+
+
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+
+    string s;
+    int len, dir = 0;
+    int x = 0, y = 0;
+    cin >> s;
+
+    len = s.size();
+
+    for(i = 0; i < len; i++) {
+        if(s[i] == 'R') {
+            dir = (dir + 1) % 4;
+        }
+        else if(s[i] == 'L') {
+            if(dir == 0) {
+                dir = 3;
+            }
+            else {
+                dir = (dir - 1) % 4;
+            }
+        }
+        else {
+            x = x + dx[dir], y = y + dy[dir];
+        }
+
+
+        if(x == 0 && y == 0) {
+            cout << i + 1;
+            break;
+        }
+    }
+
+    return 0;
+}
