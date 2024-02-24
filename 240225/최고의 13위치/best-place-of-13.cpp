1156a5d7a5f21211b8802a68cb07b061ed99186c
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-25 02:04:13 +0900

    Title: 최고의 13위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240225/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int a[25][25], n, i, j, k, cnt, maxx = -9999;
+
+int main() {
+    cin >> n;
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= n; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j + 2 <= n; j++) {
+            for(k = j; k <= j + 2; k++) {
+                if(a[i][k] == 1) cnt++;
+            }
+
+            if(maxx < cnt) {
+                maxx = cnt;
+                cnt = 0;
+            }
+        }
+    }
+
+    cout << maxx;
+    return 0;
+}
