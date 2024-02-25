fd75b04a4f7e2cbf3742c116c8d5f5ca99b28c11
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-26 08:54:31 +0900

    Title: 이상한 진수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240226/이상한 진수 2/awkward-digits-2.cpp
+++ 240226/이상한 진수 2/awkward-digits-2.cpp
@@ -3,14 +3,19 @@ using namespace std;
 
 int main() {
     string a;
+    int sw = 0;
     cin >> a;
 
-    
     for(int i = 0; i < a.size(); i++) {
         if(a[i] == '0') {
             a[i] = '1';
+            sw = 1;
             break;
         }
+    }
+
+    if(sw == 0) {
+        a[a.size() - 1] = '0';
     }
     
     int cnt = 0;
