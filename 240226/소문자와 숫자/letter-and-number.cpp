07e5da6b0f9eb883c38b7c7aec57acebdb7f1cd4
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-26 09:08:57 +0900

    Title: 소문자와 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240226/소문자와 숫자/letter-and-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] >= 'A' && s[i] <= 'Z'){
+            s[i] = s[i] + 32;
+        }
+    }
+
+    for(int i = 0; i < s.size(); i++) {
+        if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '1' && s[i] <= '9')) {
+            cout << s[i];
+        }
+    }
+
+    return 0;
+}
