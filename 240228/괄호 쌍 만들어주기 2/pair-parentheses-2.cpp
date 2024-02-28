05be80e9615ff581ccdbb9691fc6593df84d126a
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-02-28 19:16:20 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240228/괄호 쌍 만들어주기 2/pair-parentheses-2.cpp
+++ 240228/괄호 쌍 만들어주기 2/pair-parentheses-2.cpp
@@ -1,40 +1,25 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main() {
     string A;
     cin >> A;
-    
-    int len = A.size();
-    int open[len] = {0}, close[len] = {0};
-    
-    if(A[0] == '(') {
-        open[0] = 1;
+
+    vector<int> open, close;
+    int result = 0;
+    for (char c : A) {
+        if (c == '(') {
+            open.push_back(close.size());
+        } else {
+            close.push_back(open.size());
+            if(close.size() >= 2 && open.size() >= 2 && open[open.size()-2] < close[close.size()-2]) {
+                result++;
+            }
+        }
     }
-    if(A[len - 1] == ')') {
-        close[len - 1] = 1;
-    }
-    
-    for(int i = 1; i < len; i++) {
-        if(A[i] == '(')
-            open[i] = open[i - 1] + 1;
-        else
-            open[i] = open[i - 1];
-    }
-    
-    for(int i = len - 2; i >= 0; i--) {
-        if(A[i] == ')')
-            close[i] = close[i + 1] + 1;
-        else
-            close[i] = close[i + 1];
-    }
-    
-    int pairs = 0;
-    for(int i = 0; i < len - 1; i++) {
-        if(A[i] == '(')
-            pairs += close[i + 1] - open[i + 1];
-    }
-    
-    cout << pairs << "\n";
+
+    cout << result << "\n";
     return 0;
 }
