ae624962fcd0151243ba425b34c8e648f4f76014
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-03-06 11:23:46 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 240306/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -8,26 +8,25 @@ int main() {
     int N, K;
     cin >> N >> K;
 
-    // 사탕의 개수를 저장할 벡터를 초기화합니다.
-    vector<int> candies(101, 0);
+    vector<int> candies(201, 0);  // 사탕의 개수를 저장할 벡터
 
     for (int i = 0; i < N; i++) {
         int candy, location;
         cin >> candy >> location;
-        candies[location] += candy;
+        candies[location] += candy;  
     }
 
-    // 사탕의 누적 합을 구합니다.
-    vector<int> sum(101, 0);
-    sum[0] = candies[0];
-    for (int i = 1; i <= 100; i++) {
-        sum[i] = sum[i-1] + candies[i];
+    vector<int> prefix_sum(201, 0);  // 각 위치까지의 누적 합을 저장할 벡터
+    prefix_sum[0] = candies[0];
+    for (int i = 1; i <= 200; i++) {
+        prefix_sum[i] = prefix_sum[i - 1] + candies[i];
     }
 
     // 각 위치에서 [c-K, c+K] 구간의 사탕 개수를 계산하여 최대값을 찾습니다.
     int max_candy = 0;
-    for (int c = K; c <= 100 - K; c++) {
-        max_candy = max(max_candy, sum[c + K] - sum[c - K] + candies[c - K]);
+    for (int c = K; c <= 100 + K; c++) {
+        int temp = prefix_sum[min(200, c + K)] - ((c - K - 1 >= 0) ? prefix_sum[c - K - 1] : 0);
+        max_candy = max(max_candy, temp);
     }
 
     cout << max_candy;
