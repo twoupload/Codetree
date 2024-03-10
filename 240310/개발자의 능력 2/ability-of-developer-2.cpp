acf74ddeb32a0b5d45e371c1383143655c5f4c76
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-03-10 11:54:52 +0900

    Title: 개발자의 능력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240310/개발자의 능력 2/ability-of-developer-2.cpp
+++ 240310/개발자의 능력 2/ability-of-developer-2.cpp
@@ -1,36 +1,52 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
+#include <cstdlib>
+#include <algorithm>
+
+#define MAX_N 6
+
 using namespace std;
 
-// 전역 변수 선언
-int minDiff = INT_MAX; // 최소 차이를 저장할 변수
+int n = MAX_N;
+int arr[MAX_N];
 
-// 6명의 개발자 능력치를 입력받아 모든 팀 조합을 고려하여 최소 차이를 계산하는 함수
-void calculateMinDiff(vector<int>& abilities) {
-    sort(abilities.begin(), abilities.end()); // 능력치를 오름차순으로 정렬
-
-    do {
-        // 현재 순열에 따른 팀의 능력치 합 계산
-        int team1 = abilities[0] + abilities[5]; // 가장 작은 값과 가장 큰 값
-        int team2 = abilities[1] + abilities[4]; // 두 번째로 작은 값과 두 번째로 큰 값
-        int team3 = abilities[2] + abilities[3]; // 중간 값끼리
-        
-        // 세 팀 중 최대값과 최소값 찾기
-        int maxTeam = max({team1, team2, team3});
-        int minTeam = min({team1, team2, team3});
-        
-        // 최대 팀과 최소 팀 간의 차이를 계산하여 현재까지의 최소 차이와 비교
-        minDiff = min(minDiff, maxTeam - minTeam);
-    } while (next_permutation(abilities.begin(), abilities.end())); // 모든 순열에 대해 반복
+int Diff(int i, int j, int k, int l) {
+    int total_sum = 0;
+    for(int l = 0; l < n; l++)
+        total_sum += arr[l];
+    
+    // 세 번째 팀원의 합은 전체에서 첫 번째 팀원과 두 번째 팀원의 합을 빼주면 됩니다.
+    int sum1 = arr[i] + arr[j];
+	int sum2 = arr[k] + arr[l];
+    int sum3 = total_sum - sum1 - sum2;
+	
+	// 세 팀의 차이 중 최댓값을 리턴합니다.
+	int ret = abs(sum1 - sum2);
+	ret = max(ret, abs(sum2 - sum3));
+	ret = max(ret, abs(sum3 - sum1));
+	
+    return ret;
 }
 
 int main() {
-    vector<int> abilities(6); // 6명의 개발자 능력치 저장할 벡터
-    for (int& ability : abilities) {
-        cin >> ability; // 능력치 입력받기
-    }
-
-    calculateMinDiff(abilities); // 최소 차이 계산 함수 호출
-
-    cout << minDiff; // 최소 차이 출력
+    // 입력
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    // 첫 번째 팀원을 만들어줍니다.
+    int min_diff = INT_MAX;
+    for(int i = 0; i < n; i++)
+        for(int j = i + 1; j < n; j++)
+			
+			// 두 번째 팀원을 만들어줍니다.
+            for(int k = 0; k < n; k++)
+				for(int l = k + 1; l < n; l++) {
+					//첫 번째 팀원과 두 번째 팀원이 겹치는지 여부를 확인합니다.
+					if(k == i || k == j || l == i || l == j)
+						continue;
+					min_diff = min(min_diff, Diff(i, j, k, l));
+				}
+    
+    cout << min_diff;
     return 0;
 }
