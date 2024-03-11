00affde4df74d7b37b8cbc0d669a760789845c07
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-03-11 14:21:09 +0900

    Title: 개발팀의 능력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240311/개발팀의 능력/the-capabilities-of-the-development-team.cpp
+++ 240311/개발팀의 능력/the-capabilities-of-the-development-team.cpp
@@ -10,16 +10,23 @@ bool isSame(int n1, int n2, int n3) {
 }
 
 void check() {
-    int totalSum = accumulate(a, a+5, 0); // 전체 합계를 구합니다.
+    int totalSum = 0;
+    for(int i = 0; i < 5; i++) {
+        totalSum += a[i];
+    }
+
+
     for(int i = 0; i < 5; i++) {
         for(int j = i+1; j < 5; j++) {
+            
             for(int k = 0; k < 5; k++) {
-                if(k == i || k == j) continue;
                 for(int l = k+1; l < 5; l++) {
-                    if(l == i || l == j) continue;
+                    if(k == i || k == j || l == i || l == j) continue;
+
                     int team1 = a[i] + a[j];
                     int team2 = a[k] + a[l];
                     int team3 = totalSum - (team1 + team2);
+
                     if(isSame(team1, team2, team3)) {
                         int maxTeam = max({team1, team2, team3});
                         int minTeam = min({team1, team2, team3});
