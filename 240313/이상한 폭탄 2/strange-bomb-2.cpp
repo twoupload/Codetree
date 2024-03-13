8a50e61be157c9d19b262f2df34ee198da6d8a97
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-03-13 11:42:17 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240313/이상한 폭탄 2/strange-bomb-2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+
+struct BOMB {
+    int num, dis;
+};
+
+BOMB bomb[100];
+
+bool isRange(int i, int j) {
+    return abs(bomb[i].dis - bomb[j].dis) <= 3;
+}
+
+int main() {
+    cin >> n >> k;
+
+    for(int i = 0; i < n; i++) {
+        cin >> bomb[i].num;
+        bomb[i].dis = i;
+    }
+
+    int maxx = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i == j) continue;
+            if(bomb[i].num == bomb[j].num && isRange(i, j)) {
+                maxx = max(maxx, bomb[i].num);
+            }
+        }
+    }
+
+    if(maxx == INT_MIN) cout << -1;
+    else {
+        cout << maxx;
+    }
+    return 0;
+}
