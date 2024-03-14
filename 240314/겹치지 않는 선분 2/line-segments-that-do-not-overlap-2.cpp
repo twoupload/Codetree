254f0d21be6846d4258a337882be854ee360ca8e
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-03-14 23:19:45 +0900

    Title: 겹치지 않는 선분 2, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240314/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
+++ 240314/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -1,47 +1,45 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-// 선분을 표현하기 위한 구조체
-struct Line {
+struct Segment {
     int start, end;
 };
 
-// 선분의 시작점을 기준으로 정렬하기 위한 함수
-bool compareLine(const Line& a, const Line& b) {
+bool compareSegment(const Segment& a, const Segment& b) {
+    if (a.start == b.start) return a.end < b.end;
     return a.start < b.start;
 }
 
 int main() {
     int N;
-    cin >> N; // 선분의 개수 입력
+    cin >> N;
 
-    vector<Line> lines(N);
-
-    // 선분의 시작점과 끝점 입력
+    vector<Segment> segments(N);
     for (int i = 0; i < N; ++i) {
-        cin >> lines[i].start >> lines[i].end;
-    }
-
-    // 선분을 시작점 기준으로 정렬
-    sort(lines.begin(), lines.end(), compareLine);
-
-    int count = 0; // 겹치지 않는 선분의 수
-    int currentEnd = -1000001; // 현재까지 확인한 선분의 끝점 중 가장 큰 값
-
-    for (const auto& line : lines) {
-        // 현재 선분의 시작점이 이전에 확인한 선분의 끝점보다 클 경우, 겹치지 않는다.
-        if (line.start > currentEnd) {
-            count++;
-            currentEnd = line.end;
-        } else {
-            // 겹치는 경우, 겹치지 않는 선분의 끝점을 갱신
-            currentEnd = max(currentEnd, line.end);
+        cin >> segments[i].start >> segments[i].end;
+        if (segments[i].start > segments[i].end) {
+            swap(segments[i].start, segments[i].end);
         }
     }
 
-    cout << count; // 겹치지 않는 선분의 수 출력
+    sort(segments.begin(), segments.end(), compareSegment);
+
+    int currentEnd = segments[0].end;
+    bool isConnected = true;
+
+    for (int i = 1; i < N; ++i) {
+        if (segments[i].start > currentEnd) {
+            isConnected = false;
+            break;
+        }
+        currentEnd = max(currentEnd, segments[i].end);
+    }
+
+    if(isConnected) {
+        cout << 0;
+    } else {
+        cout << 1;
+    }
 
     return 0;
 }
