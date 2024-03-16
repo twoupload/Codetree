c2bf1b4a543f9edc373becb74ddc12270b95e573
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-03-16 10:12:12 +0900

    Title: 스승의 은혜 3, Time: 20ms, Memory: 0MB, Status: Passed - CodeTree

--- 240316/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 240316/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -1,64 +1,48 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 #define MAX_N 1000
 
 using namespace std;
 
 int n, b;
-
 struct PS {
     int p, s;
 };
 
 PS ps[MAX_N];
 
-int main() {
-    // 입력
-    cin >> n >> b;
+bool cmp(const PS& a, const PS& b) {
+    return a.p + a.s < b.p + b.s; // 선물 가격과 배송비의 합이 작은 순으로 정렬
+}
 
-    for(int i = 0; i < n; i++)
-        cin >> ps[i].p >> ps[i].s;
+int main() {
+    cin >> n >> b;
+    for(int i = 0; i < n; i++) cin >> ps[i].p >> ps[i].s;
 
     int ans = 0;
-    
-    // 한 명의 학생에 선물 쿠폰을 쓸 때 선물 가능한 학생의 최대 명수를 구합니다.
     for(int i = 0; i < n; i++) {
-        // i번째 학생의 선물에 쿠폰을 쓸 때 선물 가능한 학생 수를 구합니다.
-
-        // tmp배열을 만들어 i번째 학생의 선물에 쿠폰을 쓸 때
-        // 각 학생의 원하는 선물 가격을 저장합니다.
-        int tmp[MAX_N] = {};
-        int tmp2[MAX_N] = {};
+        vector<int> costs;
         for(int j = 0; j < n; j++) {
-            tmp[j] = ps[j].p;
-            tmp2[j] = ps[j].s;
+            if(i == j) // 쿠폰을 사용하는 학생
+                costs.push_back(ps[j].p / 2 + ps[j].s);
+            else // 쿠폰을 사용하지 않는 학생
+                costs.push_back(ps[j].p + ps[j].s);
         }
-        tmp[i] /= 2;
+        sort(costs.begin(), costs.end()); // 비용이 낮은 순으로 정렬
 
-        // 학생을 선물 가격 순으로 정렬합니다.
-        // 선물 가격이 가장 작은 학생부터 선물을 사 줄 때,
-        // 반드시 가장 많은 학생에게 선물을 줄 수 있습니다.
-        sort(tmp, tmp + n);
-        sort(tmp2, tmp2 + n);
-
-        
-        int student = 0;
-        int cnt = 0;
-
-        // 가장 많은 학생에게 선물을 줄 때, 그 학생 수를 구합니다.
-        // student : 선물받은 학생 수 / cnt : 현재까지 쓴 돈
-        for(int j = 0; j < n; j++) {
-            if(cnt + tmp[j] + tmp2[j] > b)
-                break;
-            cnt = cnt + tmp[j] + tmp2[j];
-            student++;
+        int sum = 0, cnt = 0;
+        for(int cost : costs) {
+            if(sum + cost <= b) { // 예산 내에서 선물을 줄 수 있는 경우
+                sum += cost;
+                cnt++;
+            }
+            else break; // 예산을 초과하는 경우
         }
-
-        ans = max(ans, student);
+        ans = max(ans, cnt); // 최대 학생 수 갱신
     }
 
     cout << ans;
-    
     return 0;
 }
