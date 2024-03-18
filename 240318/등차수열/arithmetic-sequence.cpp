4d597a6620938bc09481d945dea5dd0da35c6162
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-03-18 14:21:29 +0900

    Title: 등차수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240318/등차수열/arithmetic-sequence.cpp
+++ 240318/등차수열/arithmetic-sequence.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
+#include <algorithm>
+
+#define MAX_N 100
+#define MAX_A 100
+
 using namespace std;
 
+int n;
+int arr[MAX_N];
+
 int main() {
-    int n;
+    // 입력
     cin >> n;
 
-    vector<int> nums(n);
-    for(int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
 
-    int maxCount = 0;
+    int ans = 0;
 
-    // 각 가능한 k에 대해 등차수열을 이루는 횟수를 저장할 해시맵
-    unordered_map<int, int> countMap;
+    // 각 숫자에 대해 
+    // 등차수열의 개수를 확인합니다.
+    for(int x = 1; x <= MAX_A; x++) {
+		// 모든 쌍을 만들어 등차수열의 개수를 확인합니다.
+        int cnt = 0;
 
-    // 가능한 모든 쌍에 대해 반복
-    for(int i = 0; i < n; i++) {
-        for(int j = i + 1; j < n; j++) {
-            // k를 계산 (두 숫자의 평균)
-            int k = (nums[i] + nums[j]) / 2;
-            
-            // nums[i]와 nums[j]가 k를 중심으로 대칭이라면, 즉 등차수열을 이룬다면
-            if((nums[i] + nums[j]) % 2 == 0 && (nums[i] - k) == (k - nums[j])) {
-                // 해당 k에 대한 카운트를 1 증가
-                countMap[k]++;
-                // 최대 횟수 갱신
-                maxCount = max(maxCount, countMap[k]);
-            }
-        }
-    }
-
-    cout << maxCount << endl;
-
+        for(int i = 0; i < n; i++)
+            for(int j = i + 1; j < n; j++)
+                if(arr[i] + arr[j] == 2 * x)
+                    cnt++;
+		
+        ans = max(ans, cnt);
+	}
+    
+    cout << ans;
+	
     return 0;
 }
