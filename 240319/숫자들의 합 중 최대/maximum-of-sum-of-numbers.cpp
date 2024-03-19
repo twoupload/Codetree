86613878c665583500fc531d609a3fd05059b5fb
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-03-19 16:34:38 +0900

    Title: 숫자들의 합 중 최대, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240319/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
+++ 240319/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
@@ -2,16 +2,14 @@
 using namespace std;
 
 int adding(int num) {
-    int cnt = 0;
+    int sum = 0; // 각 자리 숫자의 합을 저장할 변수
 
-    while(num >= 10) {
-        cnt = num % 10;
-        num = num / 10;
+    while(num > 0) {
+        sum += num % 10; // 현재 숫자의 마지막 자리를 더함
+        num /= 10; // 숫자의 마지막 자리를 제거
     }
 
-    cnt += num;
-    
-    return cnt;
+    return sum;
 }
 
 int main() {
@@ -21,7 +19,7 @@ int main() {
 
     int maxx = INT_MIN;
     for(int i = x; i <= y; i++) {
-        maxx = max(maxx, adding(i));
+        maxx = max(maxx, adding(i)); // 수정된 adding 함수를 사용하여 각 자리 숫자의 합의 최댓값을 찾음
     }
 
     cout << maxx;
