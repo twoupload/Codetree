b878f7b75dcc9a6589a9420570d4445157cdad02
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-06-27 09:18:54 +0900

    Title: 독서실의 거리두기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240627/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 240627/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -1,34 +1,44 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
+int n;
+string seat;
+
+int MinDist() {
+    int min_dist = n;
+    // 둘 다 1인 곳에 대해
+    // 모든 쌍을 조사하여, 그 중 가장 가까운 거리를 구합니다.
+    for(int i = 0; i < n; i++)
+        for(int j = i + 1; j < n; j++)
+            if(seat[i] == '1' && seat[j] == '1')
+                min_dist = min(min_dist, j - i);
+    
+    return min_dist;
+}
+
 int main() {
-    int N;
-    cin >> N;
-
-    string seats;
-    cin >> seats;
-
-    int maxDistance = 0;
-    int prevOccupied = -1;
-
-    for (int i = 0; i < N; i++) {
-        if (seats[i] == '1') {
-            if (prevOccupied == -1) {
-                maxDistance = 0;
-            } else {
-                maxDistance = max(maxDistance, i - prevOccupied - 1);
-            }
-            prevOccupied = i;
+    // 입력:
+    cin >> n;
+    cin >> seat;
+    
+    int ans = 0;
+    // 들어갈 위치를 일일이 정해보며
+    // 그 상황에서 가장 가까운 사람간의 거리를 구해
+    // 가능한 경우 중 최댓값을 계산합니다.
+    for(int i = 0; i < n; i++) {
+        if(seat[i] == '0') {
+            // 비어있는 위치에 인원을 배치합니다.
+            seat[i] = '1';
+            // 가장 가까운 사람간의 거리를 구해 최댓값을 갱신해줍니다.
+            ans = max(ans, MinDist());
+            // 다시 채워졌던 값을 되돌려줍니다.
+            seat[i] = '0';
         }
     }
 
-    if (seats[N-1] == '0') {
-        maxDistance = max(maxDistance, N - prevOccupied - 1);
-    }
-
-    cout << maxDistance - 1 << endl;
-
+    cout << ans;
     return 0;
 }
