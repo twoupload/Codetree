e4f0e28a1a3e86f277492a250624274b88bb1d46
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-15 09:18:04 +0900

    Title: 가장 작은 x 찾기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240715/가장 작은 x 찾기/find-smallest-x.cpp
+++ 240715/가장 작은 x 찾기/find-smallest-x.cpp
@@ -16,30 +16,30 @@ int main() {
     int n; 
     cin >> n;
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) { // 배열에 직접 넣음
         cin >> ran[i].start >> ran[i].end;
     } 
 
-    bool sw = false;
+    bool sw = false; // while 문을 빠져나오기 위한 스위치
 
     while(sw == false) {
-        int temp = x * 2;
+        int temp = x * 2; // 처음 2를 곱하고 시작
         for(int i = 0; i < n; i++) {
-            if(temp >= ran[i].start && temp <= ran[i].end) 
+            if(temp >= ran[i].start && temp <= ran[i].end) // 범위 안에 들어가는 경우 
             {
                 count += 1;
                 temp *= 2;
             }
         }
 
-        if(count == n) sw = true;
-        else {
+        if(count == n) sw = true; // n개의 범위를 전부 만족하는 경우
+        else { // 범위 전부 불만족
             count = 0;
             x++;
         }
     }
 
-    cout << x;
+    cout << x; // 최소값 출력
 
     return 0;
 }
