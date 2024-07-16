3e931ccf35e10f2e74e6b5f4114f9461522295ff
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-16 11:51:38 +0900

    Title: 이모티콘 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240716/이모티콘 출력/print-emoticon.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << "(*´∪`)" << endl;
+    cout << "(^・ω・^)" << endl;
+    cout << "(=^･^=)";
+    return 0;
+}
