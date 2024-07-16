e9f1ad18595ceb6b00b0d2d0216b29fbe4160423
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-16 11:52:45 +0900

    Title: 4줄 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240716/4줄 출력/print-four-lines.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cout << "Height" << endl;
+    cout << "200 Cm" << endl;
+    cout << "Weight" << endl;
+    cout << "138.723 Kg";
+    return 0;
+}
