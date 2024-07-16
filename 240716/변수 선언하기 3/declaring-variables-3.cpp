3ec1ccf84aea01e29737ab6467e0d9a7a995c000
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-16 11:58:00 +0900

    Title: 변수 선언하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240716/변수 선언하기 3/declaring-variables-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+
+    a = 3, b = 6;
+    c = a + b;
+
+    cout << a << " + " << b << " = "<< c;
+    return 0;
+}
