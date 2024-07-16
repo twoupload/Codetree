57335c19af01746191fd0ba24313fc1a2ab6efa7
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-16 14:25:15 +0900

    Title: 두수의 몫과 곱, Time: 13ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240716/두수의 몫과 곱/quotient-and-multiple-two-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+
+    cout << a << " / " << b << " = " << a / b << endl;
+    cout << a << " * " << b << " = " << a * b;
+    return 0;
+}
