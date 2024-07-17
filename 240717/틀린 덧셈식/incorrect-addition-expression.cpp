079e3ed57c1eb92729f9c58a0c9af4e9eab6c445
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-17 10:22:36 +0900

    Title: 틀린 덧셈식, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/틀린 덧셈식/incorrect-addition-expression.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 123;
+    int b = 43;
+    int c = 80;
+
+    cout << a << " + " << b << " = " << c;
+    return 0;
+}
