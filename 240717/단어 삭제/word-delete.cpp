dad9db76df9b13120437e04e7dad37febc80d434
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-17 09:43:21 +0900

    Title: 단어 삭제, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240717/단어 삭제/word-delete.cpp
+++ 240717/단어 삭제/word-delete.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 int main() {
-    string word, w;
+    string word, w; // 원래 단어와 찾을 단어
     cin >> word >> w;
 
-    int idx = 0; 
+    int idx = 0; // 기본 주소를 지정한다. 
     while ((idx = word.find(w)) != string::npos) { 
-        word.erase(idx, w.size());
+        // while문이 시작할 때 idx를 지정한다. 
+        // find를 이용해 쓰레기값이 나올때까지(= 찾는 값이 없을 때)
+        word.erase(idx, w.size()); // 삭제 한다. 
     }
 
-    cout << word; 
+    cout << word; // 출력
 
     return 0;
 }
