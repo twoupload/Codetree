293d07ebf8635c4639808883b2ebcca3a85bcefe
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-17 10:24:45 +0900

    Title: 실수형 변수 선언하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240717/실수형 변수 선언하기/declare-real-variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a, b;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << a << "     " << b << "     " << a + b;
+    return 0;
+}
