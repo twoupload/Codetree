a907f6b294cca0c0ca749181ecdc75bdac5a0b56
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-17 10:19:16 +0900

    Title: 실수와 문자 받아 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/실수와 문자 받아 출력하기 2/print-number--and-letter-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char s;
+    double x, y;
+
+    cin >> s >> x >> y;
+
+    cout << fixed;
+    cout.precision(2);
+    
+    cout << y << endl;
+    cout << x << endl;
+    
+    cout << s << endl;
+    return 0;
+}
