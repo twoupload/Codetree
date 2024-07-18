5af836ad3d2c314334f4cc2eb8fe2769b053d7e2
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-18 09:49:19 +0900

    Title: 이상한 폭탄 3, Time: 8ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 240718/이상한 폭탄 3/strange-bomb-3.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, k;
+
+    vector<int> v;
+    int arr[1234567] = {};
+
+    cin >> n >> k;
+
+    for(int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(v[i] == v[j]) {
+                if(j - i <= k) {
+                    arr[v[i]] += 1;
+                }
+            }
+        }
+    }
+
+    int maxx = arr[0];
+    int maxIdx = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] >= maxx) {
+            maxx = arr[i];
+            maxIdx = i;
+        }
+    }
+
+    cout << maxIdx;
+    return 0;
+}
