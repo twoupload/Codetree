263a5ebbcf6eb59547f4c624f9a4949cc31c1f57
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-18 12:48:52 +0900

    Title: 입력받아 연산하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/입력받아 연산하기 2/take-input-and-operate-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a + 894 << endl;
+    cout << b / 10 << endl;
+    return 0;
+}
