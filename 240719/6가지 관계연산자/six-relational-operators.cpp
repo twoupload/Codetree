9099812059239a1100c522117eea190782b9ea0c
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-19 09:44:30 +0900

    Title: 6가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/6가지 관계연산자/six-relational-operators.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a, b;
+    cin >> a >> b;
+
+    if(a >= b) cout << 1 << endl;
+    else cout << 0 << endl;
+
+    if(a > b) cout << 1 << endl;
+    else cout << 0 << endl;
+
+    if(b >= a) cout << 1 << endl;
+    else cout << 0 << endl;
+
+    if(b > a) cout << 1 << endl;
+    else cout << 0 << endl;
+
+    if(b == a) cout << 1 << endl;
+    else cout << 0 << endl;
+
+    if(b != a) cout << 1 << endl;
+    else cout << 0 << endl;
+    
+    return 0;
+}
