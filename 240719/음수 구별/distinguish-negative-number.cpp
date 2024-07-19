353428251763e4456fa1459db95c180e08809163
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-19 09:50:19 +0900

    Title: 음수 구별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/음수 구별/distinguish-negative-number.cpp
@@ -0,0 +1,18 @@
+// 6
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int num;
+    cin >> num;
+
+    if(num >= 0) {
+        cout << num;
+    } else {
+        cout << num << endl;
+        cout << "minus" << endl;
+    }
+    return 0;
+}
