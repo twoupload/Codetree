b82e53abb84ca3858c66c58ac131307daac8390c
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-19 09:41:52 +0900

    Title: 두 정수값 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/두 정수값 비교/compare-two-integer-values.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+
+    if(a == b) {
+        cout << 1 << endl;
+    } else {
+        cout << 0 << endl;
+    }
+
+    if(a == b) {
+        cout << 0 << endl;
+    } else {
+        cout << 1 << endl;
+    }
+    return 0;
+}
