fd884f7387861aa91975d77a28146bee28b31ef4
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-22 09:08:39 +0900

    Title: 최대 H 점수 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240722/최대 H 점수 2/maximum-h-score-2.cpp
+++ 240722/최대 H 점수 2/maximum-h-score-2.cpp
@@ -1,65 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <unordered_set>
 
 using namespace std;
 
-int findMaxH(vector<int>& nums, int L) {
-    int N = nums.size();
-
-    // 중복 제거 및 정렬
-    unordered_set<int> uniqueNums(nums.begin(), nums.end());
-    nums.assign(uniqueNums.begin(), uniqueNums.end());
-    sort(nums.begin(), nums.end());
-
-    int maxH = 0;
-    for (int h = 1; h <= 100; ++h) {
-        int count = 0;
-        int upgradesLeft = L;
-
-        // 숫자 변경 전략: h - 1 값을 우선적으로 변경하고, 남은 횟수는 작은 수부터 변경
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == h - 1 && upgradesLeft > 0) {
-                ++nums[i];
-                --upgradesLeft;
-            } 
-            if (nums[i] >= h) {
-                ++count;
-            }
-        }
-
-        // 남은 변경 횟수를 사용하여 최대한 h 이상의 값 만들기 (가장 작은 값부터)
-        for (int i = 0; i < nums.size() && upgradesLeft > 0; ++i) {
-            if (nums[i] < h) {
-                ++nums[i];
-                --upgradesLeft;
-                ++count; 
-            }
-        }
-
-        if (count >= h) {
-            maxH = h;
-        } else {
-            // 만약 count가 h보다 작으면 h를 더 이상 높일 수 없으므로 반복문 종료
-            break; 
-        }
-    }
-
-    return maxH;
-}
-
 int main() {
-    int N, L;
-    cin >> N >> L;
+    int n, l;
+    cin >> n >> l;
 
-    vector<int> nums(N);
-    for (int i = 0; i < N; ++i) {
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
         cin >> nums[i];
     }
 
-    int maxH = findMaxH(nums, L);
-    cout << maxH << endl;
+    for (int h = n; h >= 0; h--) {
+        int lCnt = 0, hCnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= h) {
+                hCnt++;
+            } else if (nums[i] == h - 1) {
+                hCnt++;
+                lCnt++;
+            }
+        }
+
+        if (hCnt >= h && (lCnt - (hCnt - h)) <= l) {
+            cout << h << endl;
+            break;
+        }
+    }
 
     return 0;
 }
