e1876c285b682fbd26d51d990cbd50e969c0ef40
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-22 09:43:32 +0900

    Title: 시력 검사 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240722/시력 검사 3/eye-test-3.cpp
@@ -0,0 +1,14 @@
+// 2
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double right, left;
+    cin >> right >> left;
+
+    if(right >= 1.0 && left >= 1.0) cout << 'H' << endl;
+    else if((right >= 0.5 && right < 1.0) && (left >= 0.5 && left < 1.0) cout << 'M' << endl;
+    else cout << 'L' << endl;
+    return 0;
+}
