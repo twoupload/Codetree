d85b8e1bec166b9e947ee8586390047ad09ac768
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-22 09:56:21 +0900

    Title: 작은 수에서 빼기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240722/작은 수에서 빼기/subtract-from-small-number.cpp
@@ -0,0 +1,13 @@
+// 5
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> a >> b;
+
+    if(a > b) cout << b - a << endl;
+    else cout << a - b << endl;
+    return 0;
+}
