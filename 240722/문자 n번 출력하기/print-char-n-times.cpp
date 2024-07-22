26135988e22f38d68330e2cb567f34c01185b26a
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-22 10:04:46 +0900

    Title: 문자 n번 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240722/문자 n번 출력하기/print-char-n-times.cpp
@@ -0,0 +1,16 @@
+// 10
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    char c;
+
+    cin >> n >> c;
+
+    for(int i = 0; i < n; i++) {
+        cout << C;
+    }
+    return 0;
+}
