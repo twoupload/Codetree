a66d7142cbc9309786ad2344240cecf517107713
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-22 10:00:13 +0900

    Title: 재귀 속의 합계, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240722/재귀 속의 합계/sum-in-recursive-state.cpp
@@ -0,0 +1,18 @@
+// 8
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, i;
+    cin >> n;
+
+    int sum = 0;
+    while( i == n) {
+        sum += i;
+        i++;
+    }
+
+    cout << sum;
+    return 0;
+}
