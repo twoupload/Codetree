fd81ef8bbbcdf584fd73bbf932f6d77e5115d9af
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-22 10:04:06 +0900

    Title: 5부터 차례로, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240722/5부터 차례로/in-order-of-five.cpp
@@ -0,0 +1,14 @@
+// 9
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for(int i = 5; i <= n; i++) {
+        cout << i << " ";
+    }
+    return 0;
+}
