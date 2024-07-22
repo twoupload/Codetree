b5390978202be8321e8562793e5699f4764ef14d
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-22 09:58:02 +0900

    Title: 정수의 상태, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240722/정수의 상태/state-of-an-integer.cpp
@@ -0,0 +1,16 @@
+// 6
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    cin >> n;
+
+    if( n == 0 ) cout << "zero" << endl;
+    else if(n > 0) cout << "positive" << endl;
+    else cout << "negative" << endl;
+    return 0;
+}
