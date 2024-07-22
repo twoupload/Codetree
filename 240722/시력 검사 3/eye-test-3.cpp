cec2baff6ebcc1867f6a91e7320aa08e9e5e64bc
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-22 09:45:06 +0900

    Title: 시력 검사 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240722/시력 검사 3/eye-test-3.cpp
+++ 240722/시력 검사 3/eye-test-3.cpp
@@ -8,7 +8,7 @@ int main() {
     cin >> right >> left;
 
     if(right >= 1.0 && left >= 1.0) cout << 'H' << endl;
-    else if((right >= 0.5 && right < 1.0) && (left >= 0.5 && left < 1.0)) cout << 'M' << endl;
+    else if((right >= 0.5 && right < 1.0) || (left >= 0.5 && left < 1.0)) cout << 'M' << endl;
     else cout << 'L' << endl;
     return 0;
 }
