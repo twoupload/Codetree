04bfaac8db15663f4fdc7dac24bdf5bc092ae95f
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-23 22:31:44 +0900

    Title: 문자열 n번 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/문자열 n번 출력하기/output-string-n-times.cpp
@@ -0,0 +1,14 @@
+// 2
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for(int i = 0; i <n; i++) {
+        cout << "CodeTree Is Best!" << endl;
+    }
+    return 0;
+}
