47d3e9ba6698da3c67fecfb74887c22a6c18968f
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-23 22:32:53 +0900

    Title: n의 배수 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240723/n의 배수 출력/multiples-output-of-n.cpp
@@ -0,0 +1,14 @@
+// 3
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= 15; i++) {
+        cout << 7 * i << " ";
+    }
+    return 0;
+}
