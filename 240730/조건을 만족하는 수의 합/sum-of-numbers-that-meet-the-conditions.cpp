8dca3a39334f3441c5d0cf39fe56e21dfee25e7b
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-30 10:58:21 +0900

    Title: 조건을 만족하는 수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/조건을 만족하는 수의 합/sum-of-numbers-that-meet-the-conditions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    for(int i = n; i <= 500; i++) {
+        if(i % 2 == 0) {
+            sum += i;
+        }
+    }
+
+    cout << sum;
+    return 0;
+}
