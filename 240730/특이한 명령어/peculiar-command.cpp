46f41020b465d279f71a2b4bef3edf901d45edc5
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-30 10:55:22 +0900

    Title: 특이한 명령어, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/특이한 명령어/peculiar-command.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+
+    cout << fixed;
+    cout.precision(1);
+
+    for(int i = 0; i < n; i++) {
+        int a, b;
+        char c;
+
+        cin >> a >> b >> c;
+
+        if(c == 's') {
+            cout << a * b << endl;
+        } else if(c == 't') {
+            cout << ((double)a * b / 2) << endl;
+        } else {
+            continue;
+        }
+    }
+
+    return 0;
+}
