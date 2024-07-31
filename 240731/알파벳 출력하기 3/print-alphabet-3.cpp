21bf11e77cc1179e65b301aa7661b087210d48cf
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-31 10:59:48 +0900

    Title: 알파벳 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/알파벳 출력하기 3/print-alphabet-3.cpp
+++ 240731/알파벳 출력하기 3/print-alphabet-3.cpp
@@ -11,8 +11,15 @@ int main() {
     int cnt = 0;
     for(int i = 0; i < n; i++) {
         for(int j = n - i; j > 0; j--) {
-            cout << char(alp + cnt);
-            cnt++;
+            if(char(alp + cnt) == 'Z') {
+                cout << char(alp + cnt);
+                cnt = 0;
+            }
+            
+            else {
+                cout << char(alp + cnt);
+                cnt++;
+            }
         }
         cout << endl;
     }
