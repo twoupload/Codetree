c571088d72627e2c4bff181527a176c2fccf56d8
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-07-31 09:35:04 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/두 선분/two-lines.cpp
+++ 240731/두 선분/two-lines.cpp
@@ -6,16 +6,7 @@ int main() {
     int x1, x2, x3, x4;
     cin >> x1 >> x2 >> x3 >> x4;
 
-    if(x2 >= x3) {
-        cout << "intersecting" << endl;
-    } else if (x4 <= x2) {
-        cout << "intersecting" << endl;
-    } else if (x3 <= x1 && x2 <= x4) {
-        cout << "intersecting" << endl;
-    } else if (x1 <= x3 && x4 <= x2) { 
-        cout << "intersecting" << endl;
-    } else {
-        cout << "nonintersecting" << endl;
-    }
+    if(x2 < x3 || x4 < x1) cout << "nonintersecting" << endl;
+    else cout << "intersecting" << endl;
     return 0;
 }
