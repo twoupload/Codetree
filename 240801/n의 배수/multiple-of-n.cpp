cd47da81b81813e2db05f1557e67992c6e3c0a54
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-08-01 10:08:47 +0900

    Title: n의 배수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/n의 배수/multiple-of-n.cpp
@@ -0,0 +1,27 @@
+// 2
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    int i = 1;
+    while(n < 200) {
+
+        if((n * i) % 10 == 0) {
+            cout << n * i << " ";
+            i++;
+            break;
+        }
+
+        else {
+            cout << n * i << " ";
+            i++;
+        }
+    }
+
+    return 0;
+}
