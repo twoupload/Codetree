d94742383087befe1d95e8d236b5c2d83667b221
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-08-02 09:42:05 +0900

    Title: 배열의 합을 구하는 함수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240802/배열의 합을 구하는 함수/function-to-find-sum-of-arrays.cpp
+++ 240802/배열의 합을 구하는 함수/function-to-find-sum-of-arrays.cpp
@@ -3,7 +3,46 @@ using namespace std;
 
 int arr[1001][1001];
 int n;
-int s3, s4, ts;
+
+void srow() {
+    for(int i = 0; i < n; i++) {
+        int sum = 0;
+        for(int j = 0; j < n; j++) {
+            sum += arr[i][j];
+        }
+
+        arr[i][n] = sum;
+    }
+}
+
+void scol() {
+    for(int j = 0; j < n; j++) {
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += arr[i][j];
+        }
+
+        arr[n][j] = sum;
+    }
+}
+
+void slowandcol() {
+    int sum = 0;
+    for(int i = 0; i < n + 1; i++) {
+        sum += arr[n][i];
+    }
+
+    arr[n][n] = sum;
+}
+
+void show() {
+    for(int i = 0; i < n +1; i++) {
+        for(int j = 0; j < n + 1; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 
 int main() {
@@ -18,35 +57,10 @@ int main() {
         }
     }
 
-    for(int i = 0; i < n; i++) {
-        int sum1 = 0;
-        for(int j = 0; j < n; j++) {
-            sum1 += arr[i][j];
-        }
-
-        arr[i][n] = sum1;
-    }
-
-    for(int j = 0; j < n; j++) {
-        int sum2 = 0;
-        for(int i = 0; i < n; i++) {
-            sum2 += arr[i][j];
-        }
-
-        arr[n][j] = sum2;
-    }
-
-    for(int i = 0; i < n + 1; i++) {
-        s3 += arr[n][i];
-    }
-
-    arr[n][n] = s3;
-
-    for(int i = 0; i < n +1; i++) {
-        for(int j = 0; j < n + 1; j++) {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
+    srow();
+    scol();
+    slowandcol();
+    show();
+    
     return 0;
 }
