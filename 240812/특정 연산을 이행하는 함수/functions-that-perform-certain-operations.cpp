68b7799f236573bee9b2ed5618708540e6e2ac99
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-08-12 08:14:09 +0900

    Title: 특정 연산을 이행하는 함수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240812/특정 연산을 이행하는 함수/functions-that-perform-certain-operations.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    for(int i = 0; i < 3; i++) {
+        cin >> n;
+
+        if(n % 2 == 0) cout << n / 2 << endl;
+        else cout << (n * 3) - 20 << endl;
+    }
+    return 0;
+}
