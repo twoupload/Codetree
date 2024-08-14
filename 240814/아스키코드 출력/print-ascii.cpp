de25619c302027b3648eb9439acf9b26cbaae42f
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-08-14 09:25:56 +0900

    Title: 아스키코드 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240814/아스키코드 출력/print-ascii.cpp
@@ -0,0 +1,17 @@
+// 2
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        int nn;
+        cin >> nn;
+
+        cout << char(nn) << endl;
+    }
+    return 0;
+}
