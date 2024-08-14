2107c71854f5ec1cb345f895f6a75f3e27bf91bf
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-08-14 09:27:31 +0900

    Title: 문자열 세번 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240814/문자열 세번 출력하기/print-string-thrice.cpp
@@ -0,0 +1,13 @@
+/// 3
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    for(int i = 0; i < 3; i++) {
+        cout << s << endl;
+    }
+    return 0;
+}
