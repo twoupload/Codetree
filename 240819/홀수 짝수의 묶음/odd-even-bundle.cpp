79b2335e5243dd32db8b9f7c4bca12595f162e28
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-08-19 09:16:54 +0900

    Title: 홀수 짝수의 묶음, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240819/홀수 짝수의 묶음/odd-even-bundle.cpp
+++ 240819/홀수 짝수의 묶음/odd-even-bundle.cpp
@@ -4,27 +4,25 @@
 using namespace std;
 
 int n, ans = 0;
-bool is_found = false; // 변수 이름을 변경했습니다.
+bool is_found = false;
 
 void back(int index, int p, bool is_odd, const vector<int>& arr) {
-    // 각 묶음의 속한 수들의 총합을 담을 변수
     int sum = 0;
 
     for (int i = index; i < n; i++) {
-        sum += arr[i]; // 현재 원소를 더합니다.
+        sum += arr[i];
 
         if (is_found) {
-            // 답을 찾아냈음으로 재귀호출 된 함수들에서 필요없는 반복문을 돌리지 않도록 모두 탈출해줍니다.
             return;
         }
 
-        // 홀수를 만들어야 하는 차례
+        // 홀수 차례
         if (is_odd && sum % 2 != 0) {
-            back(i + 1, p + 1, !is_odd, arr); // 다음 묶음 호출
+            back(i + 1, p + 1, !is_odd, arr);
         }
         // 짝수 차례
         else if (!is_odd && sum % 2 == 0 && i != index) {
-            back(i + 1, p + 1, !is_odd, arr); // 다음 묶음 호출
+            back(i + 1, p + 1, !is_odd, arr);
         }
     }
 
@@ -44,18 +42,14 @@ int main() {
     cin >> n;
     is_found = false;
 
-    // 현재 묶음이 합이 홀수이어야 하는 경우인지 짝수이어야 하는 경우인지 구분지어주는 변수입니다
-    bool odd = false;
     vector<int> arr(n);
-
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    // 배열 탐색 시작점인 0, 현재까지의 묶음 수 0, 짝수로 시작하기 때문에 odd 변수에 false를 넣어서 재귀함수를 시작합니다
-    back(0, 0, odd, arr);
+    back(0, 0, true, arr); // 홀수로 시작
 
-    cout << ans + 1 << endl;
+    cout << ans + 1 << endl; // 답 출력
 
     return 0;
 }
