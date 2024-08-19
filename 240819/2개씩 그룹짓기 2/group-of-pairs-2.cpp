febecc7e49b69865ad449ef7b7b29c6e5e8cb588
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-08-19 10:14:40 +0900

    Title: 2개씩 그룹짓기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240819/2개씩 그룹짓기 2/group-of-pairs-2.cpp
+++ 240819/2개씩 그룹짓기 2/group-of-pairs-2.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
+#include <limits.h>
+#include <cmath>
 
 using namespace std;
 
@@ -17,8 +19,15 @@ int main() {
     // 배열을 정렬합니다.
     sort(arr.begin(), arr.end());
 
+    for(int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
     // 두 원소의 차이의 최솟값을 최대화하기 위해
-    long long min_diff = numeric_limits<long long>::max();
+    long long min_diff = LLONG_MAX; // 최대값 지정
+
     for (int i = 0; i < n; i++) {
         min_diff = min(min_diff, static_cast<long long>(arr[n + i] - arr[i]));
     }
