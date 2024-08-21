b894ea2b0a2ab52b32fc169e1f958204d2ab765f
SangDo.L <126847724+twoupload@users.noreply.github.com>
2024-08-21 10:09:15 +0900

    Title: 섞기 전 카드 위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240821/섞기 전 카드 위치/card-position-before-shuffling.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+/// 순서와 바뀐 순서에 관한 구조체
+struct o
+{
+    int to;
+    int from;
+};
+
+/// 순서
+o order[101];
+/// 사용된 순서
+o use_order[101];
+/// n번 섞은 이후의 카드
+long long card_num[101];
+
+int n, i;
+
+int main() {
+    cin >> n;
+
+    for(i = 1; i <= n; i++) 
+    {
+        int k;
+        cin >> k;
+
+        order[i].to = i;
+        order[i].from = k;
+    }
+
+    for(i = 1; i <= n; i++) 
+        cin >> card_num[i];
+
+
+    int cnt = 1;
+    for(i = 3; i >= 1; i--)
+    {
+        use_order[cnt].to = order[i].from;
+        use_order[cnt].from = order[i].to;
+        cnt++;
+    }
+
+    for(i = 1; i <= 3; i++)
+    {
+        int a, b;
+        a = card_num[use_order[i].to];
+        b = card_num[use_order[i].from];
+
+        card_num[use_order[i].to] = b;
+        card_num[use_order[i].from] = a;
+    }
+
+    for(i = 1; i <= n; i++)
+        cout << card_num[i] << endl;
+
+    return 0;
+}
